1432df95bb6b67b0f5ca0d0a4df2135063fe0a4a
R3dD3v <36352343+R3dD3v@users.noreply.github.com>
2021-03-18 19:24:45 -0500

    Fix platform specific problems

--- metasnatch.py
+++ metasnatch.py
@@ -1,11 +1,10 @@
-#   Metasnatch 0.5.7
+#   Metasnatch 0.5.8
 #   RedDevRedemption
 
 # Importing libraries lol
 import requests as rq
 from time import sleep
-import os
-import json
+import os, json
 
 a = 0
 while a == 0:
@@ -15,8 +14,8 @@ while a == 0:
     defaultStation = settings["defaultStation"]
     jsonfile.close()
     # Get input
-    os.system('clear')
-    print("\033[96m - Metasnatch 0.5.7 - \033[00m")
+    os.system('clear||cls')
+    print("\033[96m - Metasnatch 0.5.8 - \033[00m")
     print("h | help")
     station = input("Enter Station ID | ")
     if station == "":
@@ -25,17 +24,17 @@ while a == 0:
     i = 10
     err = 0
     oldtitle = "a"
-    os.system('clear')
+    os.system('cls||clear')
     if station == "h":
         while i > -1:
-            os.system('clear')
+            os.system('cls||clear')
             print("The station ID is the last 4 numbers in the link of your iHeart radio station.")
             print("iheart.com/full-metal-jackie-6225 | The ID is 6225")
             print("Hopefully that clears things up")
             print("[Clearing in " + str(i) + "]")
             i = i - 1
             sleep(1)
-        os.system('clear')
+        os.system('cls||clear')
     if station != "h":
         a = a + 1
 
@@ -50,17 +49,17 @@ while a != 0:
         title = data['title']
         err = 0
         if oldtitle != title:
-            os.system('clear')
+            os.system('cls||clear')
             print("\033[92m")
             print("-----------" + "\033[96m")
             print("Now Playing")
             print(artist + " - " + title + "\033[00m" + "\033[92m")
             print("-----------" + "\033[94m")
             print("iHeart Radio | Channel ID " + str(station) + "\033[92m")
-            print("Metasnatch 0.5.7 | RedDevRedemption")
+            print("Metasnatch 0.5.8 | RedDevRedemption")
             oldtitle = title
     elif err == 0:
-        os.system('clear')
+        os.system('cls||clear')
         print("\033[96m" + "Thanks for listening! (No Song Metadata) | " + "Channel ID " + str(station))
         print("\033[98m" + "If you're seeing this, there's either no song playing, the metadata on the api hasn't updated yet, or you entered an invalid station ID.")
         print("Worst case scenario I fucked up and my code doesn't work.")
